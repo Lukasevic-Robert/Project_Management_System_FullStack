9a739768b6369596773b568bc95abcda34f004da
LukasevicProjects <lukasevic.robert@gmail.com>
2021-05-09 21:57:59 +0300

    autocomplete user tags, CreateProject component - to functional

--- front/src/components/CreateProject.js
+++ front/src/components/CreateProject.js
@@ -1,6 +1,5 @@
-
-import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { Link, useHistory } from "react-router-dom";
 import UserService from "../services/UserService";
 import ProjectService from "../services/ProjectService";
 import swal from 'sweetalert';
@@ -14,15 +13,40 @@ import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
-import Checkbox from '@material-ui/core/Checkbox';
-import Input from '@material-ui/core/Input';
-import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { createFilterOptions } from '@material-ui/lab/Autocomplete';
+import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/core/styles";
 
 
-
+const CustomAutocomplete = withStyles({
+    tag: {
+        backgroundColor: 'transparent',
+        height: 30,
+        color: "#5c99bd",
+        fontSize: "15px",
+        variant: "outlined",
+        position: "relative",
+        border: "1px solid #be9ddf",
+    },  
+        
+})(Autocomplete);
 
 
 const theme = createMuiTheme({
+
+    overrides: {
+        MuiChip: {
+          deleteIcon: {
+            color: '#be9ddf',
+                "&:hover": {
+                  color: '#8d6dad'
+                }
+              
+          }
+        }
+      },
     palette: {
         primary: {
             main: '#7eb8da',
@@ -33,45 +57,43 @@ const theme = createMuiTheme({
             main: '#be9ddf',
             backgroundColor: '#fff',
         },
+        default: {
+            main: '#be9ddf',
+            backgroundColor: '#fff',
+        }
     },
 });
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-    PaperProps: {
-        style: {
-            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-            width: 250,
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: 400,
+        '& > * + *': {
+            marginTop: theme.spacing(3),
         },
     },
-};
+}));
 
 
+function CreateProject({ match }) {
 
-class CreateProject extends Component {
-    constructor(props) {
-        super(props)
+    const classes = useStyles();
 
-        this.state = {
-            id: this.props.match.params.id,
-            title: '',
-            content: '',
-            status: '',
-            personName: [],
-            userList: [],
-            userListId: [],
-            userData: [],
-        }
+    let history = useHistory();
+    const [id, setId] = useState(match.params.id);
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [status, setStatus] = useState('');
+    const [personName, setPersonName] = useState([]);
+    const [userList, setUserList] = useState([]);
+    const [userListId, setUserListId] = useState([]);
+    const [userData, setUserData] = useState([]);
 
-        this.saveOrUpdateProject = this.saveOrUpdateProject.bind(this);
-        this.changeTitle = this.changeTitle.bind(this);
-        this.changeStatus = this.changeStatus.bind(this);
-        this.changeContent = this.changeContent.bind(this);
-        this.handlePersonName = this.handlePersonName.bind(this);
-    }
+    const filterOptions = createFilterOptions({
+        matchFrom: 'start',
+        stringify: option => option
+    });
 
-    componentDidMount() {
+    useEffect(() => {
 
         UserService.getUsers().then((res) => {
             let users = res.data;
@@ -80,21 +102,21 @@ class CreateProject extends Component {
                 let fullname = user.firstName + ` ` + user.lastName;
                 userInfo.push(fullname);
             })
-
-            this.setState({ userList: userInfo, userData: users });
+            setUserList(userInfo);
+            setUserData(users);
 
         })
             .catch((error) => {
-                this.getErrorMessage();
-                this.props.history.push('/projects');
+                getErrorMessage();
+                history.push('/projects');
 
             });
 
-        if (this.state.id === '-1') {
+        if (id === '-1') {
             return;
         } else {
 
-            ProjectService.getProjectById(this.state.id).then((res) => {
+            ProjectService.getProjectById(id).then((res) => {
                 let project = res.data;
                 let users = [];
                 let userId = [];
@@ -102,56 +124,57 @@ class CreateProject extends Component {
                     users.push(user.firstName + ` ` + user.lastName);
                     userId.push(user.id);
                 })
-
-                this.setState({ title: project.name, status: project.status, content: project.description, personName: users, userListId: userId });
-
+                setTitle(project.name);
+                setStatus(project.status);
+                setContent(project.description);
+                setPersonName(users);
+                setUserListId(userId);
             })
                 .catch((error) => {
-                    this.getErrorMessage();
-                    this.props.history.push('/projects');
-
+                    getErrorMessage();
+                    history.push('/projects');
                 });
         }
-    }
 
+    }, [])
 
-    saveOrUpdateProject = (e) => {
+    const saveOrUpdateProject = (e) => {
         e.preventDefault();
         let userId = [];
-        this.state.userData.forEach((user) => {
+        userData.forEach((user) => {
             let username = user.firstName + ` ` + user.lastName;
-            if (this.state.personName.includes(username)) {
+            if (personName.includes(username)) {
                 userId.push(user.id);
             }
         });
 
-        this.setState({ userListId: userId });
+        setUserListId(userId);
 
-        let project = { name: this.state.title, status: this.state.status, description: this.state.content, usersId: userId };
+        let project = { name: title, status: status, description: content, usersId: userId };
         //  console.log('project=>' + JSON.stringify(project));
-        if (this.state.id === '-1') {
+        if (id === '-1') {
             ProjectService.createProject(project).then(res => {
-                this.getSuccessMessage("added");
-                this.props.history.push('/projects');
+                getSuccessMessage("added");
+                history.push('/projects');
             })
                 .catch((error) => {
-                    this.getErrorMessage();
-                    this.props.history.push('/projects');
+                    getErrorMessage();
+                    history.push('/projects');
                 }
                 );
         } else {
-            ProjectService.updateProject(project, this.state.id).then(res => {
-                this.getSuccessMessage("updated");
-                this.props.history.push('/projects');
+            ProjectService.updateProject(project, id).then(res => {
+                getSuccessMessage("updated");
+                history.push('/projects');
             })
                 .catch((error) => {
-                    this.getErrorMessage();
+                    getErrorMessage();
                 }
                 );
         }
     }
 
-    getErrorMessage() {
+    const getErrorMessage = () => {
         const errorMessage = swal({
             text: "Something went wrong! ",
             button: "Go back to project list",
@@ -161,7 +184,7 @@ class CreateProject extends Component {
         return errorMessage;
     }
 
-    getSuccessMessage(status) {
+    const getSuccessMessage = (status) => {
         const successMessage = swal({
             title: "Request successful",
             text: `The project has been ${status}`,
@@ -171,8 +194,8 @@ class CreateProject extends Component {
         return successMessage;
     }
 
-    getTitle() {
-        if (this.state.id === '-1') {
+    const getTitle = () => {
+        if (id === '-1') {
             return <h3 className="text-center">Add a new project</h3>
         }
         else {
@@ -180,116 +203,90 @@ class CreateProject extends Component {
         }
     }
 
-
-    handleChange = (e) => {
-        this.setState({
-            [e.target.id]: e.target.value
-        });
+    const handlePersonName = (event, value) => {
+        setPersonName(value);
     }
 
-    handlePersonName = (e) => {
-        this.setState({
-            personName: e.target.value
-        });
+    const changeTitle = (event) => {
+        setTitle(event.target.value);
     }
-
-    changeTitle = (event) => {
-        this.setState({ title: event.target.value });
+    const changeStatus = (event) => {
+        setStatus(event.target.value);
     }
-    changeStatus = (event) => {
-        this.setState({ status: event.target.value });
+    const changeContent = (event) => {
+        setContent(event.target.value);
     }
-    changeContent = (event) => {
-        this.setState({ content: event.target.value });
-    }
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs" className={classes.root}>
+                <ValidatorForm onSubmit={saveOrUpdateProject}>
+                    <Typography color="secondary" component="h1" variant="h5">{getTitle()}</Typography>
+                    <TextValidator
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="project"
+                        label="Project name"
+                        name="project"
+                        value={title}
+                        // autoComplete="email"
+                        validators={['required']}
+                        errorMessages={['this field is required']}
+                        onChange={changeTitle}
+                        autoFocus
+                    />
+                    <TextValidator
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="filled-textarea"
+                        label="Description"
+                        name="description"
+                        value={content}
+                        // autoComplete="email"
+                        validators={['required']}
+                        errorMessages={['this field is required']}
+                        onChange={changeContent}
+                    />
+                    {id !== '-1' ? <FormControl required id="form-control">
+                        <InputLabel id="demo-simple-select-label">Status</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={status}
+                            onChange={changeStatus}
+                        >
+                            <MenuItem value={`ACTIVE`}>ACTIVE</MenuItem>
+                            <MenuItem value={`DONE`}>DONE</MenuItem>
 
-
-
-    // handleSubmit = (e) =>{
-    //     e.preventDefault();
-    //     console.log(this.state);
-    // }
-
-    render() {
-        return (
-            <ThemeProvider theme={theme}>
-                <Container component="main" maxWidth="xs">
-
-                    <ValidatorForm onSubmit={this.saveOrUpdateProject}>
-
-                        <Typography color="secondary" component="h1" variant="h5">{this.getTitle()}</Typography>
-
-                        <TextValidator
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="project"
-                            label="Project name"
-                            name="project"
-                            value={this.state.title}
-                            // autoComplete="email"
-                            validators={['required']}
-                            errorMessages={['this field is required']}
-                            onChange={this.changeTitle}
-                            autoFocus
+                        </Select>
+                    </FormControl> : ''}
+                    <FormControl style={{ maxWidth: 'xs' }} id="form-control">
+                        <CustomAutocomplete
+                            filterOptions={filterOptions}
+                            multiple
+                            id="tags-standard"
+                            options={userList}
+                            onChange={handlePersonName}
+                            getOptionLabel={(option) => option}
+                            value={personName}
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    variant="standard"
+                                    label="Users"
+                                    placeholder="Search"
+                                />
+                            )}
                         />
-
-                        <TextValidator
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="filled-textarea"
-                            label="Description"
-                            name="description"
-                            value={this.state.content}
-                            // autoComplete="email"
-                            validators={['required']}
-                            errorMessages={['this field is required']}
-                            onChange={this.changeContent}
-
-                        />{this.state.id !== '-1' ? <FormControl required id="form-control">
-                            <InputLabel id="demo-simple-select-label">Status</InputLabel>
-                            <Select
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                value={this.state.status}
-                                onChange={this.changeStatus}
-                            >
-                                <MenuItem value={`ACTIVE`}>ACTIVE</MenuItem>
-                                <MenuItem value={`DONE`}>DONE</MenuItem>
-
-                            </Select>
-                        </FormControl> : ''}
-                        <FormControl id="form-control">
-                            <InputLabel id="demo-mutiple-checkbox-label">Users</InputLabel>
-                            <Select
-                                labelId="demo-mutiple-checkbox-label"
-                                id="demo-mutiple-checkbox"
-                                multiple
-                                value={this.state.personName}
-                                onChange={this.handlePersonName}
-                                input={<Input />}
-                                renderValue={(selected) => selected.join(', ')}
-                                MenuProps={MenuProps}
-                            >
-                                {this.state.userList.map((name) => (
-                                    <MenuItem key={name} value={name}>
-                                        <Checkbox checked={this.state.personName.indexOf(name) > -1} />
-                                        <ListItemText primary={name} />
-                                    </MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-                        <Button id="icon" variant="contained" color="primary" type="submit" style={{ marginRight: '10px' }}>Submit</Button>
-                        <Link to={'/projects'} style={{ textDecoration: 'none' }}><Button id="icon" variant="contained" color="secondary">Cancel</Button></Link>
-
-                    </ValidatorForm>
-                </Container>
-            </ThemeProvider>
-        )
-    }
+                    </FormControl>
+                    <Button id="icon" variant="contained" color="primary" type="submit" style={{ marginRight: '10px' }}>Submit</Button>
+                    <Link to={'/projects'} style={{ textDecoration: 'none' }}><Button id="icon" variant="contained" color="secondary">Cancel</Button></Link>
+                </ValidatorForm>
+            </Container>
+        </ThemeProvider>
+    )
 }
-
-export default CreateProject;
+export default CreateProject
