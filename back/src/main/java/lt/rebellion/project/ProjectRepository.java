e7549b341ec86a8ee3a36cb216d9267a49aac26e
LukasevicProjects <lukasevic.robert@gmail.com>
2021-05-15 23:39:25 +0300

    project filter implementation - backend

--- back/src/main/java/lt/rebellion/project/ProjectRepository.java
+++ back/src/main/java/lt/rebellion/project/ProjectRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,5 +17,8 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
 	
 	@Query(value = "SELECT * FROM projects WHERE user_id = ?1", nativeQuery = true)
 	List<Project> getAllProjectsByUserId(Long id);	
+	
+	@Query(value = "SELECT * FROM projects WHERE UPPER(name) LIKE %:keyword% OR UPPER(description) LIKE %:keyword%", nativeQuery = true)
+	Page<Project>findPaginatedProjectsByKeyword(Pageable pageable, @Param("keyword") String keyword);
 	
 }
