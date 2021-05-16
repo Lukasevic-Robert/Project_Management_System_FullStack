db7cb7d12387c97988374a78bd65ee15cfec3770
LukasevicProjects <lukasevic.robert@gmail.com>
2021-05-16 21:01:07 +0300

    some bugs

--- front/src/components/CreateProject.js
+++ front/src/components/CreateProject.js
@@ -245,7 +245,7 @@ function CreateProject({ match }) {
                         value={title}
                         inputProps={{ maxLength: 50 }}
                         // autoComplete="email"
-                        validators={['required']}
+                        validators={['required', 'matchRegexp:^([A-Za-z]+ )+[A-Za-z]+$|^[A-Za-z]+$']}
                         errorMessages={['this field is required']}
                         onChange={changeTitle}
                         autoFocus
